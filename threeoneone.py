b46426dbc3a77f041b5e85ee5d3f29dd730a0729
Josh <joshua.sankarlal@gmail.com>
2025-07-17 18:44:41 -0400

    Added JS clicks as fallback to Selenium click errors

--- threeoneone.py
+++ threeoneone.py
@@ -12,6 +12,13 @@ from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.chrome.options import Options
 import logging
 
+def safe_click(browser, element):
+    try:
+      element.click()
+    except Exception as e:
+       print(f"Selenium click failed with {e}, falling back to JS Click", flush=True)
+       browser.execute_script("arguments[0].click();", element)
+      
 def run_selenium(user_location, complaint_reason, accessibility_input, route_num, route_dir, stop_id, incident_date_input, incident_time_input, first_name_input, last_name_input):
     #Browser Application setup
     #browser = webdriver.Chrome()
@@ -21,6 +28,7 @@ def run_selenium(user_location, complaint_reason, accessibility_input, route_num
     options.add_argument("--no-sandbox")
     options.add_argument("-width=1920")
     options.add_argument("-height=1080")
+    #options.add_argument("--screenshot")
 
     browser = webdriver.Chrome(options=options)
     
@@ -51,25 +59,26 @@ def run_selenium(user_location, complaint_reason, accessibility_input, route_num
     #Complaint
     call_reason = browser.find_element(By.XPATH, '//mat-label[contains(text(), "Call?")]/ancestor::mat-form-field//mat-select')
     actions.move_to_element(call_reason).perform()
-    call_reason.click()
+    safe_click(browser, call_reason)
     browser.execute_script("arguments[0].scrollIntoView({block: 'center'});", call_reason)
     time.sleep(0.5)
-    call_reason.click()
-    print(" --- COMPLAINT SELECTED --- ", flush=True)
-    
+    call_reason = browser.find_element(By.XPATH, '//span[@class="mat-option-text" and normalize-space(text())="Complaint"]')
+    safe_click(browser, call_reason)
+    print(" --- COMPLAINT SELECTED --- ", flush=True) 
+    browser.get_screenshot_as_file("screenshots/screenshot.png") 
     #Its doing something weird so we need to click the body of the DOM to close the complaint window and "spawn" the "Reason for complaint"
-    browser.find_element(By.XPATH, ("//body")).click()
+    browser.find_element(By.XPATH, ("//body")).click() 
     print(" --- body selected --- ", flush=True)
- 
-    complain_wait = wait.until(EC.presence_of_element_located((By.XPATH, '//mat-label[contains(text(), "Complaint Type:")]/ancestor::mat-form-field//mat-select')))
-    
+    time.sleep(1)
+    #browser.get_screenshot_as_file("screenshots/screenshot.png")
     #Reason for Complaing
+    complain_wait = wait.until(EC.element_to_be_clickable((By.XPATH, '//mat-label[contains(text(), "Complaint Type:")]/ancestor::mat-form-field//mat-select')))
     complaint_reason_button = browser.find_element(By.XPATH,'//mat-label[contains(text(), "Complaint Type:")]/ancestor::mat-form-field//mat-select')
     actions.move_to_element(complaint_reason_button).perform()
-    complaint_reason_button.click()
+    safe_click(browser, complaint_reason_button)
     time.sleep(1)
     complaint_reason = browser.find_element(By.XPATH, f'//span[@class="mat-option-text" and normalize-space(text())="{complaint_reason}"]')
-    complaint_reason.click()
+    safe_click(browser, complaint_reason_button)
     actions.send_keys(Keys.ESCAPE)
     time.sleep
 
@@ -77,7 +86,7 @@ def run_selenium(user_location, complaint_reason, accessibility_input, route_num
     accessibility_binary = browser.find_element(By.XPATH, '//mat-label[contains(text(), "accessibility")]/ancestor::mat-form-field//mat-select')
     actions.move_to_element(accessibility_binary).perform()
     #acc = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='mat-option-44']/span")))
-    accessibility_binary.click()
+    safe_click(browser, accessibility_binary)
     #acc = WebDriverWait(browser,10).until(EC.presence_of_element_located(By.XPATH, "//*[@id='mat-option-44']/span"))
 
     time.sleep(1)
@@ -95,10 +104,10 @@ def run_selenium(user_location, complaint_reason, accessibility_input, route_num
     actions.move_to_element(route_number).perform()
     rnumber_wait = wait.until(EC.element_to_be_clickable(route_number))
     print ("--- ROUTE NUMBER CLICKABLE ---", flush=True)
-    route_number.click()
+    safe_click(browser, route_number)
     route_number_xpath = f'//span[@class="mat-option-text" and normalize-space(text())="{route_num}"]'
     route_number = browser.find_element(By.XPATH,route_number_xpath)
-    route_number.click()
+    safe_click(browser, route_number)
     actions.send_keys(Keys.TAB)
     print ("--- ROUTE NUMBER SENT ---", flush=True)
 
@@ -107,7 +116,7 @@ def run_selenium(user_location, complaint_reason, accessibility_input, route_num
     route_direction.click()
     route_direction_xpath = f'//span[@class="mat-option-text" and normalize-space(text())="{route_dir}"]'
     route_direction = browser.find_element(By.XPATH, route_direction_xpath)
-    route_direction.click()
+    safe_click(browser, route_direction)
     actions.send_keys(Keys.TAB)
     print ("--- ROUTE DIRECTION SENT ---", flush=True)   
 
@@ -130,7 +139,8 @@ def run_selenium(user_location, complaint_reason, accessibility_input, route_num
     print(incident_date_input) #Check format
     incident_date = browser.find_element(By.XPATH, '//input[contains(@aria-label, "Date of incident")]') 
     actions.scroll_to_element(incident_date).perform()
-    incident_date.click()
+    safe_click(browser, incident_date)
+    
     incident_date.send_keys(incident_date_input) #Enter in reconfigured date
     actions.send_keys(Keys.TAB)
 
