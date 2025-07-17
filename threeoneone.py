9d5659c9eef17e09b972b45056ca2720e4748fbc
Josh <joshua.sankarlal@gmail.com>
2025-07-17 19:29:28 -0400

    please

--- threeoneone.py
+++ threeoneone.py
@@ -69,8 +69,15 @@ def run_selenium(user_location, complaint_reason, accessibility_input, route_num
     browser.find_element(By.XPATH, ("//body")).click() 
     print(" --- BODY CLICK TO TRIGGER COMPLAINT TYPE --- ", flush=True)
     
-    complaint_type_button = wait.until(EC.visibility_of_element_located((By.XPATH,'//mat-label[contains(text(), "Complaint Type:")]/ancestor::mat-form-field//mat-select')))
-
+    for attempt in range (3):
+        try: 
+            complaint_type_button = wait.until(EC.visibility_of_element_located((By.XPATH,'//mat-label[contains(text(), "Complaint Type:")]/ancestor::mat-form-field//mat-select')))
+        except Exception as e:
+            print ("Complaint Type not found. Blur and Retrying...", flush=True)
+            browser.execute_script("document.activeElement.blur();")
+            time.sleep(1)
+    
+    
     browser.get_screenshot_as_file("screenshots/screenshot.png") 
     #browser.get_screenshot_as_file("screenshots/screenshot.png")
     #Reason for Complaing
